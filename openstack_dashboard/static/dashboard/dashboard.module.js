047b3fe41cea4a67e4060b9dcc65a2c3c46469b0
Thai Tran <tqtran@us.ibm.com>
2015-05-12 13:45:34 -0700

    WIP Table Demo
    Do not merge. This is for demo only.
    
    Change-Id: I6cf187c3a7ba3055c86c14b0d882667e4e61dbac

--- openstack_dashboard/static/dashboard/dashboard.module.js
+++ openstack_dashboard/static/dashboard/dashboard.module.js
@@ -3,7 +3,8 @@
 
   var module = angular.module('hz.dashboard', [
     'hz.dashboard.launch-instance',
-    'hz.dashboard.workflow'
+    'hz.dashboard.workflow',
+    'hz.dashboard.demo'
   ]);
 
   module.constant('dashboardBasePath', '/static/dashboard/');
